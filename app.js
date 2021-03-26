ce9950292125de86e61b4b224b806730132e90a8
Timusy <sumitchoudhary2686@gmail.com>
2021-03-26 07:13:14 +0530

    Securing authentication through hash

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const https=require("https");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
 const _=require("lodash");
-const encrypt=require("mongoose-encryption")
+// const encrypt=require("mongoose-encryption");
+
+var md5 = require('md5');
 
 const app=express();
 
@@ -22,7 +24,7 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User=mongoose.model("User",userSchema);
 
@@ -41,7 +43,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -55,7 +57,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
   User.findOne({email:username},function(err,foundUser){
     if(err){
       console.log(err);}
